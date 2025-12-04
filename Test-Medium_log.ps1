937b156a983e4254d15dce9a6698ae6792ca4ac7
hatterp <cyberitsec@proton.me>
2025-12-04 14:41:10 +0100

    Update Test-Medium_log.ps1

--- Test-Medium_log.ps1
+++ Test-Medium_log.ps1
@@ -1,11 +1,16 @@
 # Ransomware Simulation - Medium Intensity (with visible output)
 # Safe: does NOT encrypt real files.
 
+$logFile = "$env:TEMP\mitre_test_log.txt"
+Add-Content $logFile "`n==== Test started: $(Get-Date) ===="
+
 Write-Host "[INFO] Starting Medium-Intensity Test..." -ForegroundColor Cyan
 
 $path = "$env:TEMP\mitre_test_files"
 New-Item -ItemType Directory -Path $path -Force | Out-Null
 Write-Host "[INFO] Test folder: $path"
+
+Add-Content $logFile "[INFO] Created test file $i"
 
 $rng = [System.Security.Cryptography.RandomNumberGenerator]::Create()
 
@@ -24,4 +29,6 @@ for ($i = 0; $i -lt 200; $i++) {
     Write-Host "    -> Operation completed." -ForegroundColor Green
 }
 
+
 Write-Host "[DONE] Medium intensity ransomware simulation completed." -ForegroundColor Green
+Add-Content $logFile "[INFO] Simulation done $i"
